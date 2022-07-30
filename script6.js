c21e715ffdf6b66aa8d592e67a698eb43a9310b5
Anton <example@gmail.com>
2022-07-30 12:51:28 +0300

    firstcommit

---
+++ script6.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(4/0);
+
+const person = "Alex";
+
+const bool = true;
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+// console.log(obj.name);
+console.log(obj["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1]);
---
+++ script7.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//alert("John"); 
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(answer); 
+
+const answers = [];
+
+answers[0] = prompt("Как ваше имя?", "");
+answers[1] = prompt("Как ваше фамилия?", "");
+answers[2] = prompt("Сколько вам лет?", "");
+
+document.write(answers);
